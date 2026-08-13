197ada6f06e7ca40cb933bf5fc061872929fc555
Developer <dev@local>
2026-08-14 07:07:57 +0900

    Revert Web Remote 2D code feature as requested by user while maintaining clean NORM volume baseline and disconnect logic

--- preload.js
+++ preload.js
@@ -1,8 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-    toggleMiniMode: (isMini) => ipcRenderer.send('toggle-mini-mode', isMini),
-    getRemoteInfo: () => ipcRenderer.invoke('get-remote-info'),
-    updateRemoteStatus: (status) => ipcRenderer.send('update-remote-status', status),
-    onRemoteCommand: (callback) => ipcRenderer.on('remote-command', (event, cmd) => callback(cmd))
+    toggleMiniMode: (isMini) => ipcRenderer.send('toggle-mini-mode', isMini)
 });
